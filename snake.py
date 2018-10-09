a9d75caff1052ad0b1ce6247f766284c96c41a78
Harry Park <hg98.park@hanwha.com>
2018-10-09 17:10:03 +0900

    벽 충돌 감지 사과 먹기   먹으면 길이 늘어나기 시간지나면 빨라지기

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ import sys, pygame
 from pygame.locals import *
 
 # frame/sec definition
-TARGET_FPS = 30
+target_fps = 15
 clock = pygame.time.Clock()
 
 # Define colors
@@ -13,6 +13,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+GREY = (150, 150, 150)
 
 # mouse button define
 LEFT = 1
@@ -29,12 +30,15 @@ pygame.init()
 screen_size = (480, 320)
 tile_size = (10,10)
 screen = pygame.display.set_mode(screen_size, DOUBLEBUF)
-head_pos = (100, 100)
+head_pos = (100, 200)
 snake = [head_pos, (head_pos[0]-10, head_pos[1]), (head_pos[0]-20, head_pos[1])]
+score = 0
 
 # 
 direction = D_RIGHT
 
+TAIL_INCREASE = 3
+tail_increase_sum = 0
 def erase_old_snake():
     tail_pos = snake[len(snake)-1]
     pygame.draw.rect(screen, BLACK, (tail_pos[0], tail_pos[1], tile_size[0],tile_size[1]))
@@ -42,6 +46,40 @@ def erase_old_snake():
 def draw_snake():
     head_pos = snake[0]
     pygame.draw.rect(screen, GREEN, (head_pos[0], head_pos[1], tile_size[0],tile_size[1]))
+
+wall = []
+for i in range(100, 200, 10):
+    wall.append((i, 100))
+for brick in wall:
+    pygame.draw.rect(screen, GREY, (brick[0], brick[1], tile_size[0], tile_size[1]))
+def check_wall(snake, wall):
+    head_pos = snake[0]
+    for brick in wall:
+        if head_pos == brick:
+            # Game Over
+            # Todo : display game-over message & score 
+            pygame.quit()
+            sys.exit()
+
+apples = [(30, 30), (60, 60), (90, 90), (120, 120)]
+for apple_pos in apples:
+    pygame.draw.rect(screen, RED, (apple_pos[0], apple_pos[1], tile_size[0], tile_size[1]))
+
+def check_apple(snake, apples):
+    head_pos = snake[0]
+    global score
+    for apple_pos in apples:
+        if apple_pos == head_pos:
+            score += 10
+            print("Score = ", score)
+            del apples[apples.index(apple_pos)]
+            return TAIL_INCREASE
+    return 0
+            
+# Speed handling
+pygame.time.set_timer(pygame.USEREVENT, 10000)
+            
+            
 
 # Main Loop
 while True:
@@ -71,6 +109,10 @@ while True:
                 if direction != D_UP:
                     direction = D_DOWN
 
+        if event.type == pygame.USEREVENT:
+            print("target_pfs : ", target_fps)
+            target_fps += 2
+            if target_fps > 70: target_fps = 70
     
     erase_old_snake()
 
@@ -93,10 +135,20 @@ while True:
         if new_head_pos[1] < 0:
             new_head_pos = (head_pos[0], screen_size[1]-tile_size[1])
     snake.insert(0, new_head_pos)
-    del snake[len(snake)-1]
-       
+    if tail_increase_sum > 0:
+        tail_increase_sum -= 1
+        print("tail_increase_sum = ", tail_increase_sum)
+    else:
+        del snake[len(snake)-1]
+    
+    
+    # Collision check
+    tail_increase = check_apple(snake, apples)
+    tail_increase_sum += tail_increase
+    check_wall(snake, wall)
+        
     draw_snake()
 
     pygame.display.flip() # Update screen
-    clock.tick(TARGET_FPS) 
+    clock.tick(target_fps) 
     

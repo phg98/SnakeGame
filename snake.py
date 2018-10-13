5d071522316e2cb79ec102296d4f1de34c2c2517
Harry Park <hg98.park@hanwha.com>
2018-10-14 00:10:09 +0900

    Refactor snake into the class

--- snake.py
+++ snake.py
@@ -5,7 +5,6 @@ import sys, pygame
 from pygame.locals import *
 import random
 
-
 # Define colors
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -26,30 +25,74 @@ D_DOWN = 4
 # Initialize
 pygame.init()
 screen_size = (480, 320)
-tile_size = (10,10)
-board_size = (screen_size[0]//tile_size[0], screen_size[1]//tile_size[1])
+tile_size = (10, 10)
+board_size = (screen_size[0] // tile_size[0], screen_size[1] // tile_size[1])
 screen = pygame.display.set_mode(screen_size, DOUBLEBUF)
 score = 0
 TAIL_INCREASE = 3
 
-###### Snake handling
-def erase_old_snake(snake):
-    tail_pos = snake[len(snake)-1]
-    pygame.draw.rect(screen, BLACK, (tail_pos[0], tail_pos[1], tile_size[0],tile_size[1]))
-    
-def draw_snake(snake):
-    head_pos = snake[0]
-    pygame.draw.rect(screen, GREEN, (head_pos[0], head_pos[1], tile_size[0],tile_size[1]))
-    
-def check_snakebody(snake):
-    head_pos = snake[0]
-    for body in snake[1:]:
-        if body == head_pos:
-            # Game Over
-            # Todo : display game-over message & score 
-            pygame.quit()
-            sys.exit()
-            
+
+class Snake(list):
+    def __init__(self, a_screen, a_screen_size,  a_tile_size):
+        list.__init__([])
+        self.screen = a_screen
+        self.screen_size = a_screen_size
+        self.tile_size = a_tile_size
+        head_pos = (100, 200)
+        self.extend([head_pos, (head_pos[0] - 10, head_pos[1]), (head_pos[0] - 20, head_pos[1])])
+        self.direction = D_RIGHT
+        self.tail_increase_sum = 0
+
+    def get_head_pos(self):
+        return self[0]
+
+    def erase_old_snake(self):
+        tail_pos = self[len(self) - 1]
+        pygame.draw.rect(self.screen, BLACK, (tail_pos[0], tail_pos[1], self.tile_size[0], self.tile_size[1]))
+
+
+    def draw_snake(self):
+        head_pos = self[0]
+        pygame.draw.rect(self.screen, GREEN, (head_pos[0], head_pos[1], self.tile_size[0], self.tile_size[1]))
+
+
+    def is_body_colision(self):
+        head_pos = self.get_head_pos()
+        for body in self[1:]:
+            if body == head_pos:
+                return True
+            else:
+                return False
+
+
+    def move(self, direction):
+        head_pos = self.get_head_pos()
+        if direction == D_RIGHT:
+            new_head_pos = (head_pos[0] + 10, head_pos[1])
+            if new_head_pos[0] >= self.screen_size[0]:
+                new_head_pos = (0, head_pos[1])
+        elif direction == D_LEFT:
+            new_head_pos = (head_pos[0] - 10, head_pos[1])
+            if new_head_pos[0] < 0:
+                new_head_pos = (self.screen_size[0] - self.tile_size[0], head_pos[1])
+        elif direction == D_DOWN:
+            new_head_pos = (head_pos[0], head_pos[1] + 10)
+            if new_head_pos[1] >= self.screen_size[1]:
+                new_head_pos = (head_pos[0], 0)
+        elif direction == D_UP:
+            new_head_pos = (head_pos[0], head_pos[1] - 10)
+            if new_head_pos[1] < 0:
+                new_head_pos = (head_pos[0], self.screen_size[1] - self.tile_size[1])
+        self.insert(0, new_head_pos)
+        if self.tail_increase_sum > 0:
+            self.tail_increase_sum -= 1
+            print("tail_increase_sum = ", self.tail_increase_sum)
+        else:
+            del self[len(self) - 1]
+
+    def update_tail_increase(self, tail_increase):
+        self.tail_increase_sum += tail_increase
+
 
 ###### Wall handling
 def make_horizontal_wall(wall, startx, endx, y):
@@ -57,18 +100,22 @@ def make_horizontal_wall(wall, startx, endx, y):
         wall.append((i, y))
     return wall
 
+
 def make_boundary_wall(wall):
     for i in range(0, screen_size[0], tile_size[0]):
         wall.append((i, 0))
-        wall.append((i, screen_size[1]-tile_size[1]))
+        wall.append((i, screen_size[1] - tile_size[1]))
     for i in range(0, screen_size[1], tile_size[1]):
         wall.append((0, i))
-        wall.append((screen_size[0]-tile_size[0], i))
+        wall.append((screen_size[0] - tile_size[0], i))
     return wall
+
+
 def draw_wall(wall):
     for brick in wall:
         pygame.draw.rect(screen, GREY, (brick[0], brick[1], tile_size[0], tile_size[1]))
-        
+
+
 def check_wall(snake, wall):
     head_pos = snake[0]
     for brick in wall:
@@ -78,29 +125,32 @@ def check_wall(snake, wall):
             pygame.quit()
             sys.exit()
 
+
 ###### Apple handling
 def add_apple(apples, wall, snake):
     apple = get_new_apple(apples, wall, snake)
     apples.append(apple)
     draw_apple(apples)
 
+
 def get_new_apple(apples, wall, snake):
-    apple = (random.randint(0, board_size[0]-1), random.randint(0, board_size[1]-1))
-    apple = (apple[0]*tile_size[0], apple[1]*tile_size[1])
+    apple = (random.randint(0, board_size[0] - 1), random.randint(0, board_size[1] - 1))
+    apple = (apple[0] * tile_size[0], apple[1] * tile_size[1])
     for blocks in [apples, wall, snake]:
         for item in blocks:
             if item == apple:
                 apple = get_new_apple(apples, wall, snake)
     return apple
-    
+
+
 def draw_apple(apples):
     for apple_pos in apples:
         pygame.draw.rect(screen, RED, (apple_pos[0], apple_pos[1], tile_size[0], tile_size[1]))
-        
+
 
 def check_apple(wall, snake, apples):
     global apples_left
-    head_pos = snake[0]
+    head_pos = snake.get_head_pos()
     global score
     for apple_pos in apples:
         if apple_pos == head_pos:
@@ -114,28 +164,25 @@ def check_apple(wall, snake, apples):
             add_apple(apples, wall, snake)
             return TAIL_INCREASE
     return 0
-            
+
 
 def run_game(wall):
     global apples_left
     # frame/sec definition
     target_fps = 15
     clock = pygame.time.Clock()
-    
+
     # Init snake
-    head_pos = (100, 200)
-    snake = [head_pos, (head_pos[0]-10, head_pos[1]), (head_pos[0]-20, head_pos[1])]
     direction = D_RIGHT
-    tail_increase_sum = 0
-    draw_snake(snake)
-    
+    snake = Snake(screen, screen_size, tile_size)
+    snake.draw_snake()
+
     # Init wall
     draw_wall(wall)
-    
+
     # Init apple
     apples = []
     add_apple(apples, wall, snake)
-    
 
     # Main Loop
     while True:
@@ -145,11 +192,11 @@ def run_game(wall):
                 pygame.quit()
                 sys.exit()
 
-            if event.type == KEYDOWN:   # Press 'Q' to quit the game
+            if event.type == KEYDOWN:  # Press 'Q' to quit the game
                 if event.key == K_q:
                     pygame.quit()
                     sys.exit()
-                    
+
             if event.type == MOUSEBUTTONDOWN and event.button == LEFT:
                 print("left mouse up at (%d, %d)" % event.pos)
 
@@ -174,54 +221,38 @@ def run_game(wall):
                 print("target_pfs : ", target_fps)
                 target_fps += 2
                 if target_fps > 70: target_fps = 70
-        
-        erase_old_snake(snake)
+
+        snake.erase_old_snake()
 
         # Update state
-        head_pos = snake[0]
-        if direction ==D_RIGHT:
-            new_head_pos = (head_pos[0]+10, head_pos[1])
-            if new_head_pos[0] >= screen_size[0]:
-                new_head_pos = (0, head_pos[1])
-        elif direction ==D_LEFT:
-            new_head_pos = (head_pos[0]-10, head_pos[1])
-            if new_head_pos[0] < 0:
-                new_head_pos = (screen_size[0]-tile_size[0], head_pos[1])
-        elif direction ==D_DOWN:
-            new_head_pos = (head_pos[0], head_pos[1]+10)
-            if new_head_pos[1] >= screen_size[1]:
-                new_head_pos = (head_pos[0], 0)
-        elif direction ==D_UP:
-            new_head_pos = (head_pos[0], head_pos[1]-10)
-            if new_head_pos[1] < 0:
-                new_head_pos = (head_pos[0], screen_size[1]-tile_size[1])
-        snake.insert(0, new_head_pos)
-        if tail_increase_sum > 0:
-            tail_increase_sum -= 1
-            print("tail_increase_sum = ", tail_increase_sum)
-        else:
-            del snake[len(snake)-1]
-        
-        
+        snake.move(direction)
+
         # Collision check
         tail_increase = check_apple(wall, snake, apples)
-        tail_increase_sum += tail_increase
-        #print(apples_left)
+        snake.update_tail_increase(tail_increase)
+
+        # print(apples_left)
         if apples_left <= 0:
             print("stage cleared.")
             return True
         check_wall(snake, wall)
-        check_snakebody(snake)
-            
-        draw_snake(snake)
+        if snake.is_body_colision():
+            # Game Over
+            # Todo : display game-over message & score
+            pygame.quit()
+            sys.exit()
 
-        pygame.display.flip() # Update screen
-        clock.tick(target_fps) 
-    
+        snake.draw_snake()
+
+        pygame.display.flip()  # Update screen
+        clock.tick(target_fps)
+
+
 # Speed handling
 pygame.time.set_timer(pygame.USEREVENT, 10000)
-  
-###### Levels 
+
+
+###### Levels
 levels = []
 levels.append(make_boundary_wall([]))
 level = make_boundary_wall([])
@@ -246,4 +277,3 @@ print(len(levels), " levels ")
 print("Game End")
 pygame.quit()
 sys.exit()
-

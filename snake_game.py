069c0040a46fef60fb3781611d7ef69bb2804f2b
Harry Park <hg98.park@hanwha.com>
2018-12-27 23:33:58 +0900

    #6 Add start screen. WIP

--- snake_game.py
+++ snake_game.py
@@ -124,6 +124,51 @@ def run_game(wall, apples_left):
         pygame.display.flip()  # Update screen
         clock.tick(target_fps)
 
+def wait_for_space_input():
+    while True:
+        for event in pygame.event.get():
+            # Event handler
+            if event.type == KEYDOWN:  # Press 'space' to return
+                if event.key == K_SPACE:
+                    return
+
+def render_multi_line(text, x, y, fsize):
+    fontObj = pygame.font.Font('C:\\freesansbold.ttf', fsize)  # 현재 디렉토리로부터 myfont.ttf 폰트 파일을 로딩한다. 텍스트 크기를 32로 한다
+    lines = text.splitlines()
+    for i, l in enumerate(lines):
+        screen.blit(fontObj.render(l, 0, GREEN), (x, y + fsize * i))
+
+def start_screen():
+    # howTo = "Use arrow key to change direction of snake.\nPress 'q' to quit.\nPress space-bar to pause.[not yet implemented]\nEat apples to finish the stage.\nEat apple will makes snake grow, which makes the game more difficult.\nSnake speed will get faster as time passed.\n\nPress SPACE to start the game!"
+    howTo = '''
+    Use arrow key to change direction of snake.
+    Press 'q' to quit.
+    Press space-bar to pause.[not yet implemented]
+    Eat apples to finish the stage.
+    Eat apple will makes snake grow, makes the game more difficult.
+    Snake speed will get faster as time passed.
+    
+    Press SPACE to start the game!
+    '''
+    render_multi_line(howTo, 0, 0, 16)
+    '''
+    textSurfaceObj = fontObj.render(howTo, True, GREEN)  # 텍스트 객체를 생성한다. 첫번째 파라미터는 텍스트 내용, 두번째는 Anti-aliasing 사용 여부, 세번째는 텍스트 컬러를 나타낸다
+    textRectObj = textSurfaceObj.get_rect();  # 텍스트 객체의 출력 위치를 가져온다
+    textRectObj.center = (150, 150)  # 텍스트 객체의 출력 중심 좌표를 설정한다
+    screen.fill(BLACK)
+    screen.blit(textSurfaceObj, textRectObj)  # 설정한 위치에 텍스트 객체를 출력한다
+    '''
+    pygame.display.update()
+
+    # Font 객체 생성의 다른 예
+    #fontObj = pygame.font.Font(None, 32)  # 폰트 파일에 None을 지정할 경우 기본 폰트가 사용된다
+    #fontObj = pygame.font.Font('C:\\Windows\\Fonts\\tahoma.ttf', 32)  # 윈도우 경로에 있는 폰트를 사용할 경우
+
+    # render 함수 사용의 다른 예
+    #textSurfaceObj = fontObj.render('Hello font!', True, GREEN, BLUE)  # 텍스트 색을 녹색, 배경색을 파란색으로 설정한다
+
+    wait_for_space_input()
+
 
 # Speed handling
 pygame.time.set_timer(pygame.USEREVENT, 10000)
@@ -152,6 +197,8 @@ for level in levels:
     apples_left = 5
     print(level)
     # run_game
+    start_screen()
+    screen.fill(BLACK)
     isClear = run_game(level, apples_left)
     screen.fill(BLACK)
     if isClear is True:
@@ -167,3 +214,4 @@ print(len(levels), " levels ")
 print("Game End")
 pygame.quit()
 sys.exit()
+
